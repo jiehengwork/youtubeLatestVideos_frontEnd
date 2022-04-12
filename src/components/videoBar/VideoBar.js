f668293c66f19a5e6e96bcaa0348cd7666de964e
JieHengWork <jiehengwork@gmail.com>
2022-04-12 23:14:34 +0800

    22/04/12 videoBar改為同步方式 call api 並在完整更新過清單後才做渲染

--- src/components/videoBar/VideoBar.js
+++ src/components/videoBar/VideoBar.js
@@ -5,46 +5,64 @@ import VideoItem from "./videoItem/VideoItem";
 // css
 import { StyleVideoBarDiv } from './style';
 
+// 「同步」處理迴圈
+async function asyncForEach( array, callback ) {
+  for( let i = 0; i < array.length; i++ ) {
+    await callback(array[i]);
+  }
+}
+
 const VideoBar = () => {
   // 有新影片的頻道清單
   const [ updateArray, setUpdateArray ] = useState([]);
+  console.log('render')
 
   useEffect(() => {
-    // 訂閱的頻道清單
+    // 從 localhost 中取出 訂閱的頻道清單
     const subscriptArrayStr = localStorage.getItem('subscriptArray');
     const subscriptArray = subscriptArrayStr.split(',')
     subscriptArray.splice(0, 1);
-
-    subscriptArray.forEach( item => {
+    
+    // 暫存陣列
+    let array = []
+    // 檢查頻道是否有新影片
+    const checkNewVideo = async(item) => {
       // 取得頻道最新影片資訊
-      axios.post('http://localhost:9000/YouTubeAPI/channel/searchNewVideo', {
+      await axios.post('http://localhost:9000/YouTubeAPI/channel/searchNewVideo', {
         channelId: item
       })
       .then( res => {
-        // localStorage 資料
+        // localStorage 儲存的舊資料
         let localChannelData = localStorage.getItem(item);
         localChannelData = JSON.parse(localChannelData);
         
-        // 最新影片跟 local 資料比對，若最新影片跟 看過的影片(seen) 不同，代表是沒看過的影片
+        // 最新影片跟舊資料比對，若最新影片跟看過的影片(seen) 不同，代表是新的影片 加入更新清單
         if ( localChannelData.seen !== res.data.videoId ) {
           let channelId = localChannelData.channelId
-          setUpdateArray( current => [ ...current, channelId] )
+          array.push( channelId )
           
-          // 紀錄等等要渲染的影片ID
+          // 更新 localStorage 的資料
           localChannelData.videoId = res.data.videoId
           localChannelData.videoTitle = res.data.videoTitle
           localChannelData.videoSmallImg = res.data.videoSmallImg
           localChannelData.videoMediumImg = res.data.videoMediumImg
           localChannelData.videoBigImg = res.data.videoBigImg
-          localChannelData = JSON.stringify(localChannelData)
-          localStorage.setItem(item, localChannelData)
+          localChannelData = JSON.stringify( localChannelData )
+          localStorage.setItem( item, localChannelData )
         }
       })
-    })
+    }
+
+    // 對所有訂閱的頻道做最新影片檢查 (同步 避免多次重新渲染)
+    const checkAll = (async() => {
+      await asyncForEach( subscriptArray, checkNewVideo )
+      setUpdateArray( array )
+    })()
+
   }, [])
   
   let videoItemArray = updateArray.map( item => {
-    let itemData = localStorage.getItem(item)
+    let itemData = localStorage.getItem( item )
     itemData = JSON.parse(itemData);
     return <VideoItem data={ itemData } key={ item }/>
   })
